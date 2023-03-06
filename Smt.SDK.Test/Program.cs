491d35e64fcb04172614b21fda022a3545eda9c8
wd <3003807036@qq.com>
2023-03-06 18:03:37 +0800

    1

--- Smt.SDK.Test/Program.cs
+++ Smt.SDK.Test/Program.cs
@@ -13,17 +13,21 @@ namespace Smt.SDK.Test
     {
         static void Main(string[] args)
         {
+            SmtTemplateFreight.Create();
+        }
+        public void postproduct() 
+        {
             var Channel = "AE_GLOBAL";
             var ChannelSellerId = "2673675313";
             var accessToken = "5000050000923AawUSaCw132a369fPETCoks3rRgYbi2ckycvtgAfjuiKfKyIW1SPPt";
             var url = "https://api-sg.aliexpress.com/sync";
             string appkey = "24578631";
             string appSecret = "9de53317305962e960b1a8de288b734c";
-            IIopClient client = new IopClient(url, appkey, appSecret);
+            IIopClient client = new IopClient(url, appkey, appSecret);  
             IopRequest request = new IopRequest();
             request.SetApiName("aliexpress.postproduct.redefining.findproductinfolistquery");
             request.AddApiParameter("aeop_a_e_product_list_query", "{\"product_status_type\":\"onSelling\"}");
-            IopResponse response = client.Execute(request, accessToken,GopProtocolEnum.TOP);
+            IopResponse response = client.Execute(request, accessToken, GopProtocolEnum.TOP);
             Console.WriteLine(response.IsError());
             Console.WriteLine(response.Body);
         }
---
+++ Smt.SDK.Test/SmtTemplateFreight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Smt.SDK.Test
+{
+    public class SmtTemplateFreight
+    {
+        private string filePath = "D:\\自己的项目\\wd-0929\\Smt.SDK.Test\\Excel";
+        private SmtTemplateFreight() 
+        {
+             DirectoryInfo root = new DirectoryInfo(filePath);
+             FileInfo files = root.GetFiles().FirstOrDefault();
+        }
+        public static SmtTemplateFreight Create()
+        {
+            return new SmtTemplateFreight();
+        }
+    }
+}
